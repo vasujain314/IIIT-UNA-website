0d9478b16076550984e3b8be59b4e601180111a9
vasujain314 <jain.vasu509@gmail.com>
2018-12-09 14:26:41 +0530

    header and footer made separately along with some debugs

--- footer.php
+++ footer.php
@@ -1,74 +1,125 @@
-<style>
-footer{
-   background-color: #222222;
-   
-    text-align: center;
-    color: #CCC;
-	
-}
-
+<!DOCTYPE html>
+<head>
+	<style>
+  .footer{
+  background-color: white;
+  position: relative;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  bottom: 0px;
+}  
 
 
 
 
 </style>
-
-
-	
-	
-	
-	
-<html>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>Indian Institue Of Information Technoogy,UNA(H.P)</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Style CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.default.css" rel="stylesheet">
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" type="text/css" href="css/fontello.css">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+</head>
 <body>
-<div class="container">
-<div class="row">
-<div class="col-sm-12">
-<footer>
-
-<p><a href="index.html">Home</a>
-			&nbsp; | &nbsp;
-	<a href="aboutus.html">About Us</a>
-			&nbsp; | &nbsp;
-	<a href="academic.html">Academics</a>
-			&nbsp; | &nbsp; 
-	<a href="tender.html">Tender</a>
-			&nbsp; |  &nbsp;
-	<a href="recuritment.html">Recuritment</a>
-			&nbsp; |&nbsp;
-	<a href="grievances.html">Grivance Portal</a>
-	&nbsp; |&nbsp;
-	<a href="contact.html">Contact Us</a>
-	&nbsp; |&nbsp;
-	<a href="gallery.html">Gallery</a>
-  &nbsp; |&nbsp;
-	<a href="https://gmail.com">Login @ IIIT Una</a>
-  &nbsp; |&nbsp; 
-    <a href="http://iiitu.ac.in/hindi/">हिन्दी संस्करण </a>
-	
-	</p>
-	
-		<p>Website designed, developed & hosted by Computer Centre, National Institute of Technology, Hamirpur (H.P.) <br> 
-		&copy; Indian Institute of Information Technology, Una (Saloh), Himachal Pradesh 2018<br>
-            Website last updated on : 26/10/2018 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			Total Visitors <img src="http://hitwebcounter.com/counter/counter.php?page=6712762&style=0010&nbdigits=6&type=page&initCount=0" title="" Alt=""   border="0"> 
-            
-
-
-                                         </p>
-		   
-		   
-		   
-                                        
-                                        
-
-
-
+ <footer class="footer ">
+    <div class="container" style="color: white;">
+      <div class="row">
+        <div class="col-sm-2">
+          <ul  class="list-unstyled">
+            <li style=" padding: 7px;"><strong>LINKS</strong></li>
+            <li><a style=" padding: 7px;" href="index.html">Home</a></li>
+            <li><a style=" padding: 7px;" href="contact.html">About</a></li>
+            <li><a style=" padding: 7px;" href="feedback.html">feedback</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-3">
+          <h5><strong>Our Address</strong></h5>
+          <address>
+            FF-126,Paras Trinity<br>
+            sector 63, Gurgaon<br>
+            Delhi-NCR<br>
+            <i class="fa fa-phone fa-lg"></i>: +91 704-263-3055<br>
+            <i class="fa fa-envelope fa-lg"></i>: <a href="mailto:alrightdentalclinic@gmail.com">alrightdentalclinic@gmail.com</a>
+          </address>
+        </div>
+        <div class="col-sm-4 align-self-center">
+          <div class="text-center align-self-center">
+            <div id='printoutPanel'></div>
         
-    </footer>
-	</div>
-	</div>
-	</div>
+        <div id='myMap' style='width: auto; height: 200px;'></div>
+        <script type='text/javascript'>
+            function loadMapScenario() {
+                var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+                    /* No need to set credentials if already passed in URL */
+                    center: new Microsoft.Maps.Location(28.421883,77.109078),
+                    mapTypeId: Microsoft.Maps.MapTypeId.birdseye });
+                var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(28.421883,77.109078), { title: 'alright dental' });
+                var polyline = new Microsoft.Maps.Polyline([new Microsoft.Maps.Location(28.421883,77.109078),
+                    new Microsoft.Maps.Location(28.421883,77.109078),
+                    new Microsoft.Maps.Location(28.421883,77.109078),
+                    new Microsoft.Maps.Location(28.421883,77.109078),
+                    new Microsoft.Maps.Location(28.421883,77.109078),
+                    new Microsoft.Maps.Location(28.421883,77.109078),
+                    new Microsoft.Maps.Location(28.421883,77.109078),
+                    new Microsoft.Maps.Location(28.421883,77.109078),
+                    new Microsoft.Maps.Location(28.421883,77.109078),
+                    new Microsoft.Maps.Location(28.421883,77.109078),
+                    new Microsoft.Maps.Location(28.421883,77.109078)], { strokeColor: 'lightblue', strokeThickness: 5 });
+                var layer = new Microsoft.Maps.Layer();
+                layer.add([pushpin, polyline]);
+                map.layers.insert(layer);
+                
+            }
+        </script>
+        <script type='text/javascript' src='https://www.bing.com/api/maps/mapcontrol?key=AtzBR8MzztKkXstzT92ik-oi87ZF4kiY94t7a1awKWWD0gnnZVe90AH7QWux7QFL&callback=loadMapScenario' async defer></script>
+          </div>
+        </div>
+    <div class="col-sm-3" >
+       <ul  class="list-unstyled">
+            <li><a style=" padding: 7px;" href="contact.html">Contact us</a></li>
+            <li><a style=" padding: 7px;" href="team.html">Meet our team</a></li>
+            <li><a style=" padding: 7px;" href="quality.html">Quality</a></li>
+            <li><a style=" padding: 7px;" href="plans.html">alright plans</a></li>
+          </ul>
+    </div>
+  </div>
+  </div>
+  </footer>
+        <!-- tiny-footer -->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="tiny-footer">
+                    
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <div class="tiny-section-social">
+                            <div class="">
+                                <ul>
+                                    <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-google-plus-square"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /. tiny-footer -->
+        </div>
+    </div>
 </body>
 </html>
   
--- header.php
+++ header.php
@@ -6,279 +6,91 @@
 
 <head>
   <title>Indian Institute of Information Technology Una</title>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="./css/bootstrap.min.css">  
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-  <script src="./js/bootstrap.min.js"></script>
-<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!--[if lt IE 9]>
-  <script type='text/javascript' src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <script type='text/javascript' src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
-<![endif]-->
-<script src="./js/respond.min.js"></script>  
-
-<script type='text/javascript' src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js"></script>
-<script type='text/javascript' src="assets/js/css3-mediaqueries.js"></script> 
-
-
-
-
-
-
-
-
-
-
-
-
- 
-  <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
-    .navbar {
-      margin-bottom: 0;
-      border-radius: 0;
-    }
-    
-    /* Add a gray background color and some padding to the footer */
-    footer {
-      background-color: #f2f2f2;
-      padding: 25px;
-    }
-    
-  .carousel-inner img {
-      width: 100%; /* Set width to 100% */
-      margin: auto;
-      min-height:200px;
-  }
-
-  topb
-  {
-  height: 200px;
-  }
-.header .top-bar {
-  background: #2f506c;
-}
-.header .top-bar .search-form {
-  padding: 10px 0;
-}
-.header .top-bar .search-form .form-group {
-  display: inline-block;
-  margin-bottom: 0;
-  vertical-align: middle;
-}
-.header .social-icons li.row-end {
-  margin-right: 0;
-}
-.header .social-icons a {
-  background: #27435a;
-}
-.header .social-icons a:hover {
-  background: #6091ba;
-}
-.header .social-icons a:hover .fa {
-  color: #fff;
-}
-.header .social-icons a .fa {
-  color: rgba(255, 255, 255, 0.4);
-}
-.header .social-icons a .fa:before {
-  font-size: 18px;
-  text-align: center;
-  padding: 0;
-}
-.header .header-main {
-  padding: 30px 0;
-}
-.header h1.logo {
-  margin-top: 0;
-  margin-bottom: 0;
-}
-.header .menu-top {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  text-align: right;
-  float: right;
-}
-.header .menu-top li {
-  list-style: none;
-  float: left;
-  font-size: 11px;
-  font-family: 'open sans', arial, sans-serif;
-}
-.header .menu-top li:last-child a {
-  padding-right: 0;
-}
-.header .menu-top li.divider a {
-  border-right: 1px solid #c3c3c3;
-}
-.header .menu-top li a {
-  display: inline-block;
-  color: #777777;
-  padding: 0px 10px;
-}
-.header .menu-top li a:hover {
-  color: #6091ba;
-}
-.header .contact {
-  font-size: 20px;
-  font-family: 'open sans', arial, sans-serif;
-  font-weight: 300;
-  margin-top: 10px;
-}
-.header .contact p {
-  float: left;
-  margin-bottom: 0;
-}
-.header .contact p.phone {
-  margin-right: 30px;
-}
-.header .contact p a {
-  color: #444444;
-}
-.header .contact .fa {
-  color: #6091ba;
-  margin-right: 8px;
-  font-size: 18px;
-  position: relative;
-  top: 1px;
-}
-.header .contact .fa.fa-phone {
-  font-size: 22px;
-  top: 2px;
-}
-.header .search-form {
-  border: 0;
-  -webkit-box-shadow: none;
-  -moz-box-shadow: none;
-  box-shadow: none;
-}
-.header .search-form .form-control {
-  width: 260px;
-  background: #f5f5f5;
-  -webkit-transition: all 0.4s ease-in-out;
-  -moz-transition: all 0.4s ease-in-out;
-  -ms-transition: all 0.4s ease-in-out;
-  -o-transition: all 0.4s ease-in-out;
-}
-.header .search-form .form-control:focus {
-  background: #fff;
-  border-color: #6091ba;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /* Hide the carousel text when the screen is less than 600 pixels wide */
-  @media (max-width: 600px) {
-    .carousel-caption {
-      display: none; 
-    }
-  }
-  </style>
+      <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>Indian Institue Of Information Technoogy,UNA(H.P)</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Style CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.default.css" rel="stylesheet">
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" type="text/css" href="css/fontello.css">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
 
 
-<header class="header">  
-    <div class="header-main container">
-        <div class="col-md-2 col-sm-2">
-             <img alt="" src="images/iiituna.jpg" align="right" style="height: 100px;" class="img-circle">
+   <div class="top-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 col-md-2 col-sm-3 col-xs-12">
+                    <div class="logo"><a href="index.html"><img src="images/iiitulogo.png" alt=""></a></div>
+                </div>
+                <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12" style="padding-left: 12%;margin-top: 3%;">
+                    <h2>Indian Institute of Information Technology,Una </h2>
+                  <h3 style="padding-left: 11%;"><span> भारतीय सूचना 
+  प्रौद्योगिकी संस्थान ऊना  </span></h3>
+                    <h4 style="padding-left: 5%;">(An Institute Of National Importance Deemed By MHRD)</h4>
+                </div>
+            </div>
         </div>
-            <div class="logo col-md-9 col-sm-9"> <h2>
-				Indian Institute of Information Technology Una </h2><h3><span> भारतीय सूचना 
-प्रौद्योगिकी संस्थान ऊना  </span></h3>
-		</div>
-      
-        </h3><!--//logo-->           
-        
-    </div><!--//header-main-->
-</header><!--//header-->
-<br>
-<div class="container" style='margin-top:-30px;'>
-<nav class="navbar navbar-inverse">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      
-	  </div>
-  
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav navbar-left">
-        <li class="active"><a href="index.html">Home</a></li>
-       
-		
-		 <li class="dropdown"><a data-toggle="dropdown"
-       class="dropdown-toggle" href="#dropdown"> About US <b class="caret"></b></a>
-       <ul class="dropdown-menu">
-         <li><a data-toggle="tab" href="#dropdown1"><a href="aboutus.html">About IIIT Una</a></a></li>
-         <li><a data-toggle="tab" href="#dropdown1"><a href="mission.html">Vision & Mission</a></a></li>
-         <li><a data-toggle="tab" href="#dropdown2"><a href="adminstrative.html">Administration</a></a></li>
-         <li><a data-toggle="tab" href="#dropdown3"><a href="directordesk.html"> Director Disk </a> </a></li>  
-		 <li><a data-toggle="tab" href="#dropdown4"><a href="campus.html"> Campus  </a></li> 
-       </ul></li>
-		
-		
-		
-		
-				
-		
-		 <li class="dropdown"><a data-toggle="dropdown"
-       class="dropdown-toggle" href="#"> Departments <b class="caret"></b></a>
-       <ul class="dropdown-menu">
-         <li><a data-toggle="tab" href="#dropdown1"><a href="cse/index.html">Computer Science and Engineering</a></a></li>
-         <li><a data-toggle="tab" href="#dropdown2"><a href="ece/index.html">Electronics & Communication Engineering</a></li>
-         <li><a data-toggle="tab" href="#dropdown1"><a href="it/index.html">Information Technology</a></a></li>
-       </ul></li>
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	<li><a href="academic.html">Academics</a></li>
-	
-	<li><a href="tender.html">Tender</a></li>
-	
-	<li><a href="recuritment.html">Recuritment</a></li>
-	<li><a href="grievances.html">Grivance Portal</a></li>
-	
-		<li><a href="contact.html">Contact us</a></li>
-	<li><a href="gallery.html">Gallery</a></li>
-        <li><a href="https://gmail.com">Login @ IIIT Una</a></li>
-      <li><a href="http://iiitu.ac.in/hindi">हिन्दी</a></li>
-      </ul>
-      
     </div>
-</nav>
-  </div>
+    <!-- /.top-bar -->
+    <!-- header-section-->
+    <div class="header-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <!-- navigations-->
+                    <div class="navigation">
+                        <div id="navigation">
+                            <ul>
+                                <li class="active"><a href="index.html">Home</a></li>
+                               
+                                
+                                <li class="has-sub"><a href="#">About Us</a>
+                                    <ul>
+                                        <li><a href="aboutus.html">About IIIT UNA</a></li>
+                                        <li><a href="mission.html">Vision &amp; Misson</a></li>
+                                        <li><a href="blog-default.html">Administration</a></li>
+                                        <li><a href="blog-default.html">Director's Desk</a></li>
+                                        <li><a href="blog-default.html">Campus</a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-sub" ><a href="#">Departments</a>
+                                    <ul>
+                                        <li><a href="testimonial.html">Computer Science</a> </li>
+                                        <li><a href="404-page.html">Information Techology</a> </li>
+                                        <li><a href="faq-tab.html">Electronics &amp; Communication</a> </li>
+                                        
+                                    </ul>
+                                </li>
+                                
+                                <li><a href="about.html">Academics</a></li>
+                                <li><a href="webhosting.html">Tenders</a></li>
+                                <li><a href="webhosting.html">Recuritment</a></li>
+                                
+                                
+                                    <li><a href="contact-us.html">Gallery</a>
+                                        <li><a href="contact-us.html">LOGIN @IIIT UNA</a>
+                                            <li><a href="contact-us.html">हिन्दी</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /.navigations-->
+                </div>
+            </div>
+        </div>
+    </div>
   
   
   
