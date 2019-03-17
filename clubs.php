f34521a8840006f242baec889d74edf061c2e9bd
vasujain314 <jain.vasu509@gmail.com>
2019-03-17 18:08:50 +0530

    clubs updated

---
+++ clubs.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/jpg" href="images/iiitulogo.png" />
+       <meta name="description" content="Indian institute of information technology,Una himachal pradesh,indian a deemed university by mhrd, an institute of national importance">
+    <meta name="keywords" content="UNA ,una,Himachal Pradesh,college,indian institue of information technology,hamirpur,nith ,nit,cse,it,ece">
+    <title>Indian Institue Of Information Technoogy,UNA(H.P)</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="css/owl.theme.default.css" rel="stylesheet">
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css' integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU' crossorigin='anonymous'>
+    <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+    <style>
+/* Paste this css to your style sheet file or under head tag */
+/* This only works with JavaScript, 
+if it's not present, don't show loader */
+.no-js #loader { display: none;  }
+.js #loader { display: block; position: absolute; left: 100px; top: 0; }
+.se-pre-con {
+	position: fixed;
+	left: 0px;
+	top: 0px;
+	width: 100%;
+	height: 100%;
+	z-index: 9999;
+	background: url(images/preloader.gif) center no-repeat #fff;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
+
+
+</style>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+<script>
+	//paste this code under head tag or in a seperate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
+</script>
+
+    <script>
+$(function(){
+$("#header").load("header.php");
+$("#footer").load("footer.php");
+});
+    </script>
+
+
+</head>
+
+<body>
+    <!-- top-bar -->
+    <div class="se-pre-con"></div>
+
+ <div id="header"></div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-3 col-xs-12" align="center">
+      <img style="margin-top: -30px;" class="clublogo img-fluid" src="images/force.jpg">
+    </div>
+    <div class="col-sm-9 col-xs-12">
+      <p style="text-align: center; margin-top: 40px;">FORCE -Forum of Computer Engineers is the association of computer science in IIIT Una.
+Our main objective is to encourage open source software,improving coding culture,project building and introducing new platforms to work on. It provides opportunity for students to broaden their knowledge in the field of computer science and interact with other student who have shared interest.<a href=""> (know more...)</a> </p>
+    </div>
+  </div>
+  <br/>
+    <div class="row">
+    <div class="col-sm-3 col-xs-12" align="center">
+      <img class="clublogo img-fluid" src="images/hash.png">
+    </div>
+    <div class="col-sm-9 col-xs-12">
+      <p style="text-align: center; margin-top: 40px;">Technology-that's the middle name of our institute IIIT Una.The technical club of Department of Computer Science TEAM HASH has been started with the objective of development and exchange of relevant knowledge to learn and develop skills in various aspects of programming and become aware of the technological advancements in various spheres of computer science <a href=""> (know more...)</a> </p>
+    </div>
+  </div>
+<br/>
+ <div class="row">
+    <div class="col-sm-3 col-xs-12" align="center">
+      <img style="height: 220px; width: 100%;" class="clublogo img-fluid" src="images/datascience.png">
+    </div>
+    <div class="col-sm-9 col-xs-12">
+      <p style="text-align: center; margin-top: 40px;">This is the club of IIIT UNA, where we are working for elevating the culture of Machine Learning and Data Science. As we all know that 'AI is the new electricity'. Unlike ever before, Data Science and Artificial Intelligence are on a boom right now and this field is making more progress than many other fields combined. Seeing the ongoing trend, this club focuses on building awareness and sharing knowledge about the same.<a href=""> (know more...)</a>  </p>
+    </div>
+  </div>
+ <br/>
+ 
+<br/>
+<div class="row">
+    <div class="col-sm-3 col-xs-12" align="center">
+      <img style="margin-top: -30px;" class="clublogo img-fluid" src="images/infosec.png">
+    </div>
+    <div class="col-sm-9 col-xs-12">
+      <p style="text-align: center; margin-top: 40px;">InfoSec Community is an association at Indian Institute of Information Technology, Una which aims at bolstering the Cybersecurity culture among the college students. The basic motive of this community is to train enthusiasts in different areas of Cybersecurity and to encourage them into participating in CTFs happening in different parts of India and worldwide.<a href=""> (know more...)</a> </p>
+    </div>
+  </div>
+<br/>
+<div class="row">
+    <div class="col-sm-3 col-xs-12" align="center">
+      <img  class="clublogo img-fluid" src="images/sports.jpg">
+    </div>
+    <div class="col-sm-9 col-xs-12">
+      <p style="text-align: center; margin-top: 40px;">lorem ipsum loves to play football but on PC.<a href=""> (know more...)</a> </p>
+    </div>
+  </div>
+<br/>
+<div class="row">  
+    <div class="col-sm-3 col-xs-12" align="center">
+      <img style="margin-top: 30px;"  class="clublogo img-fluid" src="images/avesh.png">
+    </div>
+    <div class="col-sm-9 col-xs-12">
+      <p style="text-align: center; margin-top: 40px;">Aavesh is an electronics society that strives to teach and help students acquire new skills in an era of rapidly evolving technology in the field of electronics and communication engineering. The Society aims to provide potential minds of IIITU a platform to discuss innovative ideas and nurture their skills to tackle real life challenges in the world today and contribute to the technical society through various projects, patents and research publications.<a href=""> (know more...)</a> </p>
+    </div>
+  </div>
+<br/>
+<div class="row">
+    <div class="col-sm-3 col-xs-12" align="center">
+      <img  class="clublogo img-fluid" src="images/surge.png">
+    </div>
+    <div class="col-sm-9 col-xs-12">
+      <p style="text-align: center; margin-top: 60px;">SURGE is a electronics club which is established by Electronic society Aavesh in 2019. The Society aims to provide potential minds of IIITU a platform to discuss innovative ideas and nurture their skills to tackle real life challenges in the world today and contribute to the technical society through various projects, patents and research publications. <a href=""> (know more...)</a> </p>
+    </div>
+  </div>
+<br/>
+
+
+
+</div>
+
+
+
+
+    <div id="footer"></div>
+</body>
+
+</html>
